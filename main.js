df404492c248ad848517d17c35124ed594bcb391
forelleblau <marceladam@gmx.ch>
2019-02-12 22:01:42 +0100

    control.stufe

--- main.js
+++ main.js
@@ -26,6 +26,12 @@ var statcmdi = [
   [0x07, 0xF0, 0x00, 0xDD, 0x00, 0x8A, 0x07, 0x0F],
   [0x07, 0xF0, 0x00, 0x0D, 0x00, 0xBA, 0x07, 0x0F]
 ];
+var statesetcmd = [
+  [0x07, 0xF0, 0x00, 0x99, 0x01, 0x01, 0x48, 0x07, 0x0F],
+  [0x07, 0xF0, 0x00, 0x99, 0x01, 0x02, 0x49, 0x07, 0x0F],
+  [0x07, 0xF0, 0x00, 0x99, 0x01, 0x03, 0x4A, 0x07, 0x0F],
+  [0x07, 0xF0, 0x00, 0x99, 0x01, 0x04, 0x4B, 0x07, 0x0F]
+];
 var statcmdS = ["statTemp", "statVent", "statBetrH", "statByp"];
 var statcmdL = statcmdi.length;
 var calli = 0;
@@ -64,6 +70,18 @@ function startAdapter(options) {
   adapter.on('stateChange', function(id, state) {
     // Warning, state can be null if it was deleted
     adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
+    //adapter.log.debug("Adapter=" + adapter.toString());
+
+    if (!id || state.ack) return; // Ignore acknowledged state changes or error states
+    id = id.substring(adapter.namespace.length + 1); // remove instance name and id
+    state = state.val;
+    adapter.log.debug("id=" + id);
+    if (id === "control.stufe") {
+      adapter.log.debug("Setzte Stufe: " + state);
+
+      callcomfoair(statesetcmd[state-1]);
+    }
+
 
     // you can use the ack flag to detect if it is status (true) or command (false)
     if (state && !state.ack) {
@@ -152,7 +170,10 @@ function callcomfoair(hexout) {
     adapter.log.debug('Received: ' + buff.toString('hex'));
     buffarr = [...buff];
     adapter.log.debug('Received arr: ' + buffarr);
-    readComfoairData(buffarr);
+    if (buffarr.length > 3) {
+      readComfoairData(buffarr);
+    }
+
     client.destroy(); // kill client after server's response
 
   });
