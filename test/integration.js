02d603af6d4611be134671d432d9da370089175b
forelleblau <44144152+forelleblau@users.noreply.github.com>
2021-12-05 22:28:14 +0100

    v1.1.5

--- test/integration.js
+++ test/integration.js
@@ -4,4 +4,4 @@ const {
 } = require('@iobroker/testing');
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.integration(path.join(__dirname, '..')), {allowedExitCodes: [11]});
+tests.integration(path.join(__dirname, '..'));
